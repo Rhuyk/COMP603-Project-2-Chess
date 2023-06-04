c5807cd2f6cfa6f1262f48073612940fb7cd7694
PJ <goinmc@gmail.com>
2023-06-05 03:03:42 +1200

    Added background and draw , resign, quit option to the GUI

--- src/Chess_Project_2/ChessPanel.java
+++ src/Chess_Project_2/ChessPanel.java
@@ -9,9 +9,15 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -33,6 +39,7 @@ public class ChessPanel extends JPanel {
     private Player player2;
     private Player currentPlayer;
     private boolean whiteTurn = true;
+    private Image backgroundImage;
     
     private JTextField player1NameField;
     private JTextField player2NameField;
@@ -41,12 +48,22 @@ public class ChessPanel extends JPanel {
     public ChessPanel() 
     {
         setLayout(new BorderLayout());
-        setPreferredSize(new Dimension(704 + 100, 565));
+        setPreferredSize(new Dimension(820, 623));
         board = new PiecesOnBoard();
         availableMoves = new boolean[8][8];
         createPlayerLoginScreen();
-        addMouseListener();
         
+        addMouseListener();
+        loadBackgroundImage();
+    }
+    
+    private void loadBackgroundImage() {
+        try {
+            backgroundImage = ImageIO.read(new File("b2.png"));
+        } catch (IOException e) 
+        {
+            System.out.println("File Error! IOException");
+        } 
     }
     
     private void addMouseListener() {
@@ -58,10 +75,10 @@ public class ChessPanel extends JPanel {
                 {
                     int col = e.getX() / CELL_SIZE;
                     int row = (BOARD_SIZE - 1) - e.getY() / CELL_SIZE;
-                    
+                   // currentPlayer = player1; // REMOVE
                     if (col >= 0 && col < BOARD_SIZE && row >= 0 && row < BOARD_SIZE) {
                         Piece clickedPiece = board.getBoard()[col][row];
-
+                        
                         if (selectedPiece == null) {
                             if (clickedPiece != null && clickedPiece.getColour() == currentPlayer.getColourPiece()) {
                                 selectedPiece = clickedPiece;
@@ -110,7 +127,7 @@ public class ChessPanel extends JPanel {
         startButton = new JButton("Start Game");
         
         // Set font for labels and button
-        Font labelFont = new Font(Font.SANS_SERIF, Font.BOLD, 14);
+        Font labelFont = new Font(Font.SANS_SERIF, Font.BOLD, 13);
         player1Label.setFont(labelFont);
         player2Label.setFont(labelFont);
         startButton.setFont(labelFont);
@@ -138,6 +155,7 @@ public class ChessPanel extends JPanel {
             }
         });
         JPanel loginPanel = new JPanel();
+        loginPanel.setLayout(new BoxLayout(loginPanel, BoxLayout.Y_AXIS));
         
         // Add player login components to the panel
         loginPanel.add(player1Label);
@@ -152,10 +170,12 @@ public class ChessPanel extends JPanel {
     protected void paintComponent(Graphics g) 
     {
         super.paintComponent(g);
-       
+        
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        
         drawChessBoard(g);
         
-        int frameWidth = 4; 
+        int frameWidth = 5; 
         int boardWidth = BOARD_SIZE * CELL_SIZE;
         int boardHeight = BOARD_SIZE * CELL_SIZE;
 
@@ -165,16 +185,18 @@ public class ChessPanel extends JPanel {
         g.fillRect(CELL_SIZE - frameWidth - 59, CELL_SIZE - frameWidth - 58, boardWidth + 2 * frameWidth, frameWidth); // TOP
         g.fillRect(CELL_SIZE - frameWidth - 59, CELL_SIZE + boardHeight  - 58, boardWidth + 2 * frameWidth, frameWidth);
         g.fillRect(CELL_SIZE + boardWidth - 59, CELL_SIZE - frameWidth - 58, frameWidth, boardHeight + 2 * frameWidth);
-        if (player1 == null || player2 == null) 
+        if (player1 == null || player2 == null) // Check if player has logged in
         {
-            // Draw a translucent overlay if players haven't logged in yet
-            g.setColor(new Color(255, 255, 255, 200));
-            g.fillRect(CELL_SIZE - 59, CELL_SIZE - 59, BOARD_SIZE * CELL_SIZE, BOARD_SIZE * CELL_SIZE);
+            g.setColor(new Color(0, 0, 0, 200));
+            g.fillRect(CELL_SIZE - 59, CELL_SIZE - 58, BOARD_SIZE * CELL_SIZE, BOARD_SIZE * CELL_SIZE);
         }
+        
         if(availableMoves != null)
         {
             drawAvailableMoves(g);
+            
         }
+        
         
     }
 
@@ -271,62 +293,60 @@ public class ChessPanel extends JPanel {
         this.player2 = player2;
     }
 
-    private void createGameButtons() 
+private void createGameButtons() {
+    JButton resignButton = new JButton("Resign");
+    JButton drawButton = new JButton("Draw");
+    JButton restartButton = new JButton("Restart");
+    JButton quitButton = new JButton("Quit");
+
+    resignButton.addActionListener((ActionEvent e) -> {
+        int response = JOptionPane.showConfirmDialog(null, currentPlayer.getPlayerName() + ", do you wish to resign?", "Resign", JOptionPane.YES_NO_OPTION);
+        if (response == JOptionPane.YES_OPTION) {
+            JOptionPane.showMessageDialog(null, currentPlayer.getPlayerName() + " has resigned. This game has ended via resignation");
+        }
+    });
+
+    drawButton.addActionListener((ActionEvent e) -> {
+        Player otherPlayer = (currentPlayer == player1) ? player2 : player1;
+        int response = JOptionPane.showConfirmDialog(null, currentPlayer.getPlayerName() + " asks for a draw. " + otherPlayer.getPlayerName() + ", do you accept the draw?", "Draw Proposal", JOptionPane.YES_NO_OPTION);
+
+        if (response == JOptionPane.YES_OPTION) {
+            JOptionPane.showMessageDialog(null, "The game has ended via draw.");
+        } else {
+            JOptionPane.showMessageDialog(null, "Player 2 declined the draw.");
+        }
+    });
+
+    restartButton.addActionListener((ActionEvent e) -> {
+        resetGame();
+    });
+
+    quitButton.addActionListener((ActionEvent e) -> {
+        System.out.println("Thank you for playing");
+        System.exit(0);
+    });
+
+    JPanel buttonPanel = new JPanel();
+    buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+    buttonPanel.add(Box.createVerticalGlue());
+    buttonPanel.add(resignButton);
+    buttonPanel.add(drawButton);
+    buttonPanel.add(restartButton);
+    buttonPanel.add(quitButton);
+    buttonPanel.add(Box.createVerticalGlue());
+    add(buttonPanel, BorderLayout.EAST);
+}
+    
+    private void resetGame() 
     {
-        JPanel buttonPanel = new JPanel();
-        JButton resignButton = new JButton("Resign");
-        JButton drawButton = new JButton("Draw");
-        JButton saveButton = new JButton("Save");
-        JButton quitButton = new JButton("Quit");
-        
-        Font buttonFont = new Font(Font.SERIF, Font.BOLD, 14);
-        resignButton.setFont(buttonFont);
-        drawButton.setFont(buttonFont);
-        saveButton.setFont(buttonFont);
-        quitButton.setFont(buttonFont);
-        
-        resignButton.addActionListener((ActionEvent e) -> 
-        {
-            int response = JOptionPane.showConfirmDialog(null, currentPlayer.getPlayerName()+", do you wish to resign?", "Resign", JOptionPane.YES_NO_OPTION);
-            if(response == JOptionPane.YES_OPTION)
-            {
-                JOptionPane.showMessageDialog(null, currentPlayer.getPlayerName() + " has resigned. This game has ended via resignation");
-            }
-            
-        });
-
-        drawButton.addActionListener((ActionEvent e) -> 
-        {
-            // Ask the other play for a draw
-            int response = JOptionPane.showConfirmDialog(null, currentPlayer.getPlayerName()+" asks for a draw. Player 2, do you accept the draw?", "Draw Proposal", JOptionPane.YES_NO_OPTION);
-
-            if (response == JOptionPane.YES_OPTION) 
-            {
-                JOptionPane.showMessageDialog(null, "The game has ended via draw.");
-            } 
-            else 
-            {
-                JOptionPane.showMessageDialog(null, "Player 2 declined the draw.");
-            }
-
-        });
-
-        saveButton.addActionListener((ActionEvent e) -> 
-        {
-            
-        });
-
-        quitButton.addActionListener((ActionEvent e) -> 
-        {
-            System.out.println("Thank you for playing");
-            System.exit(0);
-        });
-
-        buttonPanel.add(resignButton);
-        buttonPanel.add(drawButton);
-        buttonPanel.add(saveButton);
-        buttonPanel.add(quitButton);
-
-        add(buttonPanel, BorderLayout.AFTER_LINE_ENDS);
+        board.clearAllPieces();
+        board.resetBoardAndPieces();
+        board.refreshBoard();
+        selectedRow = -1;
+        selectedCol = -1;
+        availableMoves = new boolean[8][8];
+        whiteTurn = true;
+        currentPlayer = player1;
+        repaint();
     }
 }
